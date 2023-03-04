6254c717d8e8448a715ca12f2aaae64ea5e6f62d
ScriptCamilo <script.camilo@gmail.com>
2023-03-04 15:32:37 -0300

    feat: third class finished

---
+++ classes/class03/lib/shared/utils.js
@@ -0,0 +1,24 @@
+export function supportWorkerType() {
+  let supports = false;
+  const tester = {
+    get type() { supports = true }
+  }
+
+  try {
+    new Worker('blob://', tester).terminate();
+  } finally {
+    return supports;
+  }
+}
+
+export function prepareRunChecker({ timerDelay }) {
+  let lastEvent = Date.now();
+  return {
+    shouldRun() {
+      const result = (Date.now() - lastEvent) > timerDelay;
+      if(result) lastEvent = Date.now();
+
+      return result;
+    }
+  }
+}
